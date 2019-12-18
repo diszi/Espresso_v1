28d43b362b6dbdfb60954a9cdd65d1c0b5088e52
szidonia.laszlo <szidonia.laszlo@directtwo.hu>
2019-12-18 15:05:42 +0100

    Initial commit

---
+++ app/src/main/java/d2/hu/espresso_v1/SecondActivity.java
@@ -0,0 +1,17 @@
+package d2.hu.espresso_v1;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView viewById = (TextView) findViewById(R.id.resultView);
+        Bundle inputData = getIntent().getExtras();
+        String input = inputData.getString("input");
+        viewById.setText(input);
+    }
+}
